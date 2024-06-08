fe1df947c75de4998c831805cbba3f4ebbde9b0c
gabichagassantos@hotmail.com <gabichagassantos@hotmail.com>
2024-06-08 03:37:02 -0300

    atualização temp-page interface

--- src/App.js
+++ src/App.js
@@ -1,88 +1,86 @@
-import "./App.css";
 import { useState } from "react";
-// import Background from "./Assets/Images/Mário Mundi.png";
-// import Card from "./Components/Card/Card";
+import "./App.css";
+import Card from "./Components/Card/Card";
 
 function App() {
-  const [stateTemperatura, setStateTemperatura] = useState(30);
+  const [stateTemperatura, setStateTemperatura] = useState();
   const [descricao, setDescricao] = useState("");
-  const [cidade, setCidade] = useState("São Paulo");
+  const [cidade, setCidade] = useState("");
+  const [previsao, setPrevisao] = useState([]);
+  const diasSemana = ["Domingo","Segunda-feira","Terça-feira","Quarta-feira","Quinta-feira","Sexta-feira","Sábado","Domingo","Segunda-feira","Terça-feira","Quarta-feira","Quinta-feira","Sexta-feira","Sábado",];
+  const now = new Date();
+  const currentDay = now.getDay();
 
   const callApi = () => {
-    //api openweather
-
-    console.log("vai dar um salve na Api Temperatura");
+    // api openweather
     fetch(
-      `"https://api.openweathermap.org/data/2.5/weather?q=${cidade}&lang=pt_br&appid=5c3a255d1b982d430759586a662da1f1&units=metric"`
+      `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&lang=pt_br&units=metric&appid=afb5fd1ea152ce949db3fa511fc4ce34`
     )
       .then((resposta) => {
         return resposta.json();
       })
       .then((dadoTemperatura) => {
+        // console.log(temperatura);
         setDescricao(dadoTemperatura.weather[0].description);
         setStateTemperatura(dadoTemperatura.main.temp);
-        //console.log(dadoTemperatura.main.temp);
       })
       .catch(() => {
-        alert("cidade não encontrada");
+        alert("Cidade não encontrada");
       });
+
+    //api forecast
+    fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${cidade}&lang=pt_br&units=metric&appid=afb5fd1ea152ce949db3fa511fc4ce34`
+    )
+      .then((resposta) => {
+        return resposta.json();
+      })
+      .then((dadoPrevisao) => {
+        console.log(dadoPrevisao);
+        const tempList = [];
+        for (let i = 7; i < 40; i += 8) {
+          tempList.push({
+            temp: Math.round(dadoPrevisao.list[i].main.temp),
+            clima: dadoPrevisao.list[i].weather[0].description,
+          });
+        }
+        setPrevisao(tempList);
+        console.log(tempList);
+      })
+      .catch(() => alert("Cidade não encontrada"));
   };
+
   const dadoEntrada = (evento) => {
-    setCidade(evento.target.value); // consigo pegar a tecla
+    setCidade(evento.target.value);
   };
 
   return (
     <div className="App">
-      <div className="search">
-        <input
-          className="barra"
-          placeholder="Buscar Local"
-          type="text"
-          onChange={dadoEntrada}
-        ></input>
-        <button className="Butao" onClick={callApi}>
-          Buscar
-        </button>
+      <header>
+      <h1>Clima Tempo</h1>
+      </header>
+      <div id="barra">
+        <h2>Digite abaixo o nome da cidade que deseja buscar</h2>
+      <input type="text" placeholder="Buscar Cidade" onChange={dadoEntrada}></input>
+      <button onClick={callApi}>Buscar</button>
       </div>
-      <div className="temp">
-        <p>{cidade}</p>
-        <p>{stateTemperatura}</p>
-        <p>{descricao}</p>
+      <h1>Temperatura atual</h1>
+      <Card diaDaSemana={"Hoje"} cidade={cidade} temperatura={stateTemperatura + "°"} descricao={descricao}></Card>
+      <h1>Próximos cinco dias</h1>
+      <div style={{ display: "flex" }}>
+        {[...Array(5)].map((_, index) => {
+          return (
+            <Card
+              key={index}
+              diaDaSemana={diasSemana[currentDay + index + 1]}
+              cidade={cidade}
+              temperatura={previsao[index] ? previsao[index].temp + "°" : ""}
+              descricao={previsao[index] ? previsao[index].clima : ""}
+            ></Card>
+          );
+        })}
       </div>
     </div>
   );
 }
-
 export default App;
-
-// const diasSemana = [
-//   "Domingo",
-//   "Segunda-Feira",
-//   "Terça-Feira",
-//   "Quarta-Feira",
-//   "Quinta-Feira",
-//   "Sexta-Feira",
-//   "Sábado",
-// ];
-
-// let [contadorEstado, setContadorEstado] = useState(0);
-// let contador = 0;
-
-// const contaClique = () => {
-//   console.log (contador = contador + 1);
-
-// }
-
-// return (
-//   <div className="App">
-//     <button onClick={() =>{setContadorEstado(contadorEstado = contadorEstado + 1)}}> Clique Aqui </button>
-//     <h1>{contadorEstado}</h1>
-//     <button onClick={contaClique}>Clique aqui</button>
-//     <h1>{contador}</h1>
-//     <Titulo descricao="Previsão do tempo para os próximos 7 dias"></Titulo>
-
-//     {diasSemana.map((dia) => {
-//       return <Card diaDaSemana={dia}></Card>;
-//     })}
-//   </div>
-// );
